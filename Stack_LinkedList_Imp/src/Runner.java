12c783ea884ebbf361d0899806e3ea731bef5cce
Nasrullah Hussaini <Nasrullah Hussaini@10.24.172.18>
2019-02-16 15:22:20 -0800

    Runner updated

--- Stack_LinkedList_Imp/src/Runner.java
+++ Stack_LinkedList_Imp/src/Runner.java
@@ -8,7 +8,7 @@ public class Runner {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Stack stack=new Stack();
-		//stack.push(5);
+		stack.push(5);
 		//stack.push(9);
 		//stack.push(1);
 		//stack.push(10);
